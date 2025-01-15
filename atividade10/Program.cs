4ad380175a48c83dbaa19371a2abed92abb7e654
HENRIQUE LIMA LOPES <henrique.llopes1@senacsp.edu.br>
2025-01-15 14:22:04 -0300

    updated

---
+++ atividade10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita ao usuário para digitar um número
+        Console.Write("Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Verifica se o número é par ou ímpar usando o operador %
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar.");
+        }
+    }
+}
